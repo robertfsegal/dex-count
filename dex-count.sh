31dd73d019fc74b9db0edb36d434b73790795d0e
Robert Segal <rob@getsetgames.com>
2016-08-16 17:49:10 -0400

    Create dex-count.sh

---
+++ dex-count.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+totalmethods=0
+for jar in `find . -type f -name "*.jar"`; do
+echo $jar
+dx --dex --output=temp.dex $jar 2> /dev/null
+jarmethods=$(cat temp.dex | head -c 92 | tail -c 4 | hexdump -e '1/4 "%d\n"')
+echo $jarmethods
+totalmethods=$(($a+$totalmethods))
+echo $totalmethods
+done
